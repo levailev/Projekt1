1116abff556e344fc92595f417606617155f3503
levailev <levailevente84@gmail.com>
2021-12-08 14:33:56 +0100

    Implement sensor

--- app/src/main/java/com/example/light/MainActivity.kt
+++ app/src/main/java/com/example/light/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.light
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var brightness: Sensor? = null
+    private lateinit var text: TextView
+    private lateinit var pb: CircularProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setUpSensor() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    private fun brightness(brightness: Float): String {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+            val light1 = event.values[0]
+
+            text.text = "Sensor: $light1\n${brightness(light1)}"
+            pb.setProgressWithAnimation(light1)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 }
